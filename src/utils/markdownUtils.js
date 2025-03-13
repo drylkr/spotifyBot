ce6de7d028adc7442afdffb24e9bfe7a49076614
drylkr <doughryl@gmail.com>
2025-03-13 19:24:42 +0800

    Update markdownUtils.js

--- src/utils/markdownUtils.js
+++ src/utils/markdownUtils.js
@@ -1,5 +1,5 @@
 // Escape special characters for Telegram MarkdownV2
 export function escapeMarkdown(text) {
     if (!text) return "";
-    return text.replace(/([_*\[\]()~`>#+\-=|{}.!])/g, "\\$1");
+    return text.replace(/([_*\[\]()~`>#+\-=|{}.!\\])/g, "\\$1");
 }
